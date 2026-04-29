377d23a02aa1f2b59bcbb643905b23a825c50dcc
paipcas <jfcc95@gmail.com>
2026-04-28 23:59:04 -0500

    Corrección del listado

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -32,6 +32,8 @@
 import { ref, onMounted } from "vue"
 import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
 const list = ref([])
 
 onMounted(async () => {
